3bc732fb9e8cdc3cd326f9d14387e2fad8d94563
amankumar89 <amankumaroo784@gmail.com>
2026-01-05 18:49:21 +0530

    chore: update CORS configuration in server setup
    - Modified CORS settings to restrict origin to "http://localhost:5173".
    - Enabled credentials and specified allowed methods and headers for enhanced security.

--- server/src/index.js
+++ server/src/index.js
@@ -8,7 +8,15 @@ import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true, // VERY IMPORTANT
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
